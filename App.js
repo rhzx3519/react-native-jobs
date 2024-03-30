be060de18fa851844497b58b7474209c424e9505
Zhenghao Lou <rhzx3519@gmail.com>
2024-03-30 23:46:45 +1100

    setup home page

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useState, useCallback } from 'react';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+
+import { COLORS, icons, images, SIZES } from './constants';
+import {
+  Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome
+} from './components';
+
+
+const HomeScreen = () => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <ScrollView
+      style={styles.container(insets)}
+    >
+      <Welcome 
+      
+      />
+
+      <Popularjobs
+
+      />
+
+      <Nearbyjobs
+
+      />
+    </ScrollView>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    DMBold: require('./assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('./assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('./assets/fonts/DMSans-Regular.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+  onLayoutRootView()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name='Home'
+            component={HomeScreen}
+            options={{
+              headerStyle: { backgroundColor: COLORS.lightWhite },
+              headerShadowVisible: false,
+              headerLeft: () => (
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+              ),
+              headerRight: () => (
+                <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+              ),
+              headerTitle: ""
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: (insets) => ({
+    paddingTop: insets.top,
+    paddingBottom: insets.bottom,
+    paddingLeft: insets.left,
+    paddingRight: insets.right,
+  }),
 });
---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { 
+  View, 
+  Text,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  TextInput
+} from 'react-native'
+
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+import { useNavigation } from '@react-navigation/native';
+
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"]
+
+const Welcome = () => {
+  const navigation = useNavigation();
+  const [activeJobType, setActiveJobType] = useState('Full-time')
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=''
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes} 
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item)
+                // navigation.push(`/search/${item}`)
+              }}
+            >
+              <Text
+                style={styles.tabText(activeJobType, item)}
+              >
+                {item}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
